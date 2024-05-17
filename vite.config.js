e42c16e17f38297d04e1184a5a82baee2c64f353
Roman <4romangrekk@gmail.com>
2024-05-17 13:50:49 +0300

    fix for github pages

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/Krusty-Krab-REACT/', 
   plugins: [react()],
-})
-
-// https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   css: {
-//     modules: true,
-//   },
-// });
+});
